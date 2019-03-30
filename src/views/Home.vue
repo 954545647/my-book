253a9a03e006089e61ac99d2d6df790e3968a026
954545647 <954545647@qq.com>
2019-03-30 10:23:47 +0800

    init-pro

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="read"></div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Epub from "epubjs";
+global.ePub = Epub;
+const DOWNLOAD_URL = "/三国演义.epub";
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  name: "home",
+  methods: {
+    showEpub() {
+      // 生成book
+      this.book = new Epub(DOWNLOAD_URL);
+      // 生成 rendition 通过 Book.renderTo渲染
+      this.rendition = this.book.renderTo("read", {
+        width: window.innerWidth,
+        height: window.innerHeight
+      });
+      // 通过 Rendion.display渲染电子书
+      this.rendition.display();
+    }
+  },
+  mounted() {
+    this.showEpub();
   }
-}
+};
 </script>
